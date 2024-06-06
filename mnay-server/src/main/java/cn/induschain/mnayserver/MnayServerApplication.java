b937b0481a47c63b93ea4295f5e618a184f913da
zzx <835781192@qq.com>
2024-06-06 18:00:32 +0800

    2024-06-06：也许不许耶：项目初始化

---
+++ mnay-server/src/main/java/cn/induschain/mnayserver/MnayServerApplication.java
@@ -0,0 +1,13 @@
+package cn.induschain.mnayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MnayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MnayServerApplication.class, args);
+    }
+
+}
---
+++ mnay-server/src/test/java/cn/induschain/mnayserver/MnayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.induschain.mnayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MnayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
